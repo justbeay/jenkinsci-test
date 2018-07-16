0e3f1d7678fba773665067267c346de31060dc1d
robert.li <robert.li@supernovacompanies.biz>
2018-07-16 14:59:16 +0800

    add test

--- src/com/github/justbeay/Helper.groovy
+++ src/com/github/justbeay/Helper.groovy
@@ -1,9 +1,15 @@
 #!groovy
 package com.github.justbeay;
 
+def aaa = "hello"
+
 def info(message) {
     sh "echo current dir is: `pwd`"
     echo "INFO: ${message}"
+}
+
+def testAaa() {
+    echo "== aaa: ${aaa} =="
 }
 
 return this
