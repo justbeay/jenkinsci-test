64f2a84df609a60a1c2c899997361fc51cf7a209
robert.li <robert.li@supernovacompanies.biz>
2018-08-07 17:03:38 +0800

    var test

--- src/com/github/justbeay/Helper.groovy
+++ src/com/github/justbeay/Helper.groovy
@@ -13,10 +13,10 @@ def info(message) {
 
 def var1 = 'init'
 def setVar1(value) {
-    var1 = value
+    this.var1 = value
 }
 def getVar1() {
-    return var1
+    return this.var1
 }
 
 return this
